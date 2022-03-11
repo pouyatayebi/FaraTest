df0cd43bf42bfd4be0d5235b59401716fc8c6bf2
agile-developer-js <86706746+agile-developer-js@users.noreply.github.com>
2022-03-11 05:26:07 +0330

    deploy vercel 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import { ColorModeScript } from "@chakra-ui/react";
-import * as React from "react";
+import React  from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +6,6 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ColorModeScript />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
@@ -16,9 +14,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
